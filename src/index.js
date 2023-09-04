7b4f42d6c247ebc19c7855f8395b1cb8a9426ee5
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-04 07:36:44 -0500

    to production

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { connectDB } from './db.js'
 connectDB();
 if (process.env.NODE_ENV === 'production') {
     app.listen(process.env.PORT)
+    console.log('server on port', process.env.PORT)
 } else {
     app.listen(process.env.PORT_DEV)
+    console.log('server on port', process.env.PORT_DEV)
 }
 
 
 
-console.log('server on port', 3002)
