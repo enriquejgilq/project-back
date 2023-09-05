75da62ffea5cd54e2f09a827e521c13a9635ca85
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-05 07:59:42 -0500

    fix1.0.7

--- src/db.js
+++ src/db.js
@@ -5,8 +5,7 @@ export const connectDB = async () => {
     try {
         if (process.env.NODE_ENV === 'production') {
             try {
-                await mongoose.connect(`mongodb+srv://project-backdb:TdSX0KDzk9LR1Bca@cluster0.edch4di.mongodb.net/project-backdb?retryWrites=true&w=majority`, {
-
+                await mongoose.connect(process.env.MONGODB, {
                     useNewUrlParser: true,
                     useUnifiedTopology: true,
                 })
