48e828d0efa1de0d8efe9a7b9b8957eff2d4ea5f
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-04 21:55:41 -0500

    fix1.0.4

--- src/db.js
+++ src/db.js
@@ -4,11 +4,14 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         if (process.env.NODE_ENV === 'production') {
-            await mongoose.connect(`${process.env.NAME_DB}:${process.env.PASSDB}@${process.env.CLUSTER}/${process.env.NAME_DB_COMPLEMENT}`, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            });
-
+            try {
+                await mongoose.connect(`${process.env.NAME_DB}:${process.env.PASSDB}@${process.env.CLUSTER}/${process.env.NAME_DB_COMPLEMENT}`, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                })
+            } catch (error) {
+                console.log(error);
+            }
         } else {
             await mongoose.connect(`${process.env.BD_DEV}`)
         }
